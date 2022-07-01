48c7672804d0dadb99ccdf4be919807438f4720e
AVacAdok <avakuang8@gmail.com>
2022-07-01 00:50:02 +0000

    refactor code for getAllProperties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -133,23 +133,24 @@ exports.getAllReservations = getAllReservations;
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
   `;
   //add WHERE when options has any value pair
-  if (Object.keys(options).length) {
-      queryString += ' WHERE ';
-    }
+  // if (Object.keys(options).length) {
+  //     queryString += ' WHERE 1=1 ';
+  //   }
 
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `city LIKE $${queryParams.length} `;
+    queryString += ` AND city LIKE $${queryParams.length} `;
   }
 
   if (options.owner_id) {
-    if(queryParams.length){
-    queryString += ` AND `
-  }
+  //   if(queryParams.length){
+  //   queryString += ` AND `
+  // }
     queryParams.push(options.owner_id);
-    queryString += `owner_id = $${queryParams.length}`;
+    queryString += ` AND owner_id = $${queryParams.length}`;
   }
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
@@ -158,16 +159,18 @@ exports.getAllReservations = getAllReservations;
     // }
     queryParams.push(options.minimum_price_per_night*100);
     queryParams.push(options.maximum_price_per_night*100);
-    queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}`;
+    queryString += ` AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}`;
   }
   queryString += `
-  GROUP BY properties.id `
+  GROUP BY properties.id 
+  `;
 
   if(options.minimum_rating){
+
     queryParams.push(options.minimum_rating);
     queryString += `HAVING avg(rating) >= $${queryParams.length}`;
   }
-  console.log('options',options)
+  console.log('options',options);
 
   queryParams.push(limit);
   queryString += `
